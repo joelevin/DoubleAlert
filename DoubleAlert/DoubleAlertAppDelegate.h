71921448034f50ad02a173c77b3f7759eef9983b
joelevin <joe.levin@gmail.com>
2012-07-12 14:40:42 -0400

    Initial Commit

---
+++ DoubleAlert/DoubleAlertAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DoubleAlertAppDelegate.h
+//  DoubleAlert
+//
+//  Created by Joe Levin on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DoubleAlertViewController;
+
+@interface DoubleAlertAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DoubleAlertViewController *viewController;
+
+@end
---
+++ DoubleAlert/DoubleAlertViewController.h
@@ -0,0 +1,13 @@
+//
+//  DoubleAlertViewController.h
+//  DoubleAlert
+//
+//  Created by Joe Levin on 7/12/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoubleAlertViewController : UIViewController
+
+@end
